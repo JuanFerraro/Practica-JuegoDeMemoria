373c0fb368a633dc8358cdeb34685932a1e06b5b
Juan Ferraro <juansebastianferraro@gmail.com>
2023-04-14 19:37:16 -0500

    Comenzando juego de memoria.

---
+++ app.js
@@ -0,0 +1,11 @@
+//Necesitamos:
+
+const section = document.querySelector("section");
+const cuentaVidasJugador = document.querySelector("span");
+const vidasJugador = 5;
+
+//Link de texto
+cuentaVidasJugador.textContent = vidasJugador;
+
+/* Generacion de datos: Nombre e imagen */
+[{imgSrc: './images/'}]
